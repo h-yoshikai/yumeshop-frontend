6044d349e79616011a99a3e7ca70230c3def6229
Hiroki Yoshikai <exbass11@gmail.com>
2022-09-26 14:01:45 +0900

    feat: Tagコンポーネントの作成

---
+++ src/components/atoms/Tag/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import { colors, fonts, fontSizes } from 'src/styles/Tokens';
+import styled from 'styled-components';
+
+export type TagProps = {
+  label: string;
+  color: string;
+};
+
+const Wrapper = styled.div<Pick<TagProps, 'color'>>`
+  display: inline-block;
+  padding: 0px 4px;
+  font-size: ${fontSizes.fontSize12};
+  background-color: ${(props) => props.color};
+  text-align: center;
+  color: ${colors.White};
+  font: ${fonts.NotoSansJP};
+`;
+
+export const Tag: FC<TagProps> = (props) => {
+  const { label, color } = props;
+  return <Wrapper color={color}>{label}</Wrapper>;
+};
