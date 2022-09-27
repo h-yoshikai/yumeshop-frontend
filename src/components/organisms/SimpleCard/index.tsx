885db48c68258105701dcbf1cd1d2a6c0dddc427
Hiroki Yoshikai <exbass11@gmail.com>
2022-09-27 16:17:45 +0900

    style: フォントの適用

--- src/components/organisms/SimpleCard/index.tsx
+++ src/components/organisms/SimpleCard/index.tsx
@@ -10,7 +10,7 @@ export type SimpleCardType = {
 
 const Wrapper = styled.div`
   display: inline-block;
-  font: ${fonts.NotoSansJP};
+  font-family: ${fonts.NotoSansJP};
   font-size: ${fontSizes.fontSize12};
   overflow: hidden;
   box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
