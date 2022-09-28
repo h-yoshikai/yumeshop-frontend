ae7323ef94a018da27b2516ad156c0e525b72bf3
Hiroki Yoshikai <exbass11@gmail.com>
2022-09-28 13:29:19 +0900

    refactor: 不要な記述の削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import { Carousel } from 'src/components/organisms/Carousel';
-
 import { Container, Main, Title, TokenTest } from 'src/styles/Home';
 
 const Home: NextPage = () => (
@@ -20,15 +18,6 @@ const Home: NextPage = () => (
         <Title>Welcome to Yumeshop</Title>
       </TokenTest>
     </Main>
-
-    <Carousel
-      images={[
-        'http://placehold.jp/700x400.png?text=1',
-        'http://placehold.jp/700x400.png?text=2',
-        'http://placehold.jp/700x400.png?text=3',
-        'http://placehold.jp/700x400.png?text=4',
-      ]}
-    />
   </Container>
 );
 
